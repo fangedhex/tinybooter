229366da2da6f688a811f1badbb061aba55a5f6a
Sylvain VISTE <viste.sylvain@gmail.com>
2021-01-16 14:07:05 +0100

    Updates and fixes

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,16 @@
 # Build environment
-FROM debian:buster-slim AS build-env
-RUN apt-get update && apt-get install -y --no-install-recommends build-essential cmake
+FROM alpine AS build-env
+RUN apk add --no-cache build-base clang cmake
+ENV CC=clang CXX=clang++
+#RUN apt-get update && apt-get install -y --no-install-recommends build-essential cmake
 ADD . /src/
 RUN mkdir -p /src/build && \
-cd /src/build && \
-cmake -DCMAKE_BUILD_TYPE=Release .. && \
-make
+  cd /src/build && \
+  cmake -DCMAKE_BUILD_TYPE=Release .. && \
+  make
 
 # Runtime environment
-FROM debian:buster-slim
+FROM alpine
 LABEL maintainer="Sylvain VISTE <viste.sylvain@gmail.com>"
 COPY --from=build-env /src/build/main/tinybooter /src/build/healthcheck/healthcheck /bin/
 RUN mkdir -p /etc/tinybooter.d/
--- main/include/CliService.hpp
+++ main/include/CliService.hpp
@@ -4,26 +4,28 @@
 
 #pragma once
 
+#include <cstring>
 #include <vector>
 #include <thread>
 
 #include "Service.hpp"
 //#include "shared.h"
 
+typedef unsigned short u_short;
 class CliService
 {
-    ILogger& logger;
-    std::vector<Service> &services;
-    std::thread *th;
-    int sock;
-    void thread_func();
-    bool keep_running;
-    CliService(std::vector<Service> &services, ILogger& logger);
+  ILogger &logger;
+  std::vector<Service> &services;
+  std::thread *th;
+  int sock;
+  void thread_func();
+  bool keep_running;
+  CliService(std::vector<Service> &services, ILogger &logger);
 
-    public:
-    CliService(std::string path, std::vector<Service> &services, ILogger& logger);
-    CliService(u_short port, std::vector<Service> &services, ILogger& logger);
-    ~CliService();
-    void launch();
-    void kill();
+public:
+  CliService(std::string path, std::vector<Service> &services, ILogger &logger);
+  CliService(u_short port, std::vector<Service> &services, ILogger &logger);
+  ~CliService();
+  void launch();
+  void kill();
 };
